df87362c4ff991e7185755f0cb1702dfd05e66c0
akriti <akritikts@gmail.com>
2016-07-08 17:04:41 +0530

    Search updated

--- app/src/main/java/in/silive/clime/Models/GetCityLocation.java
+++ app/src/main/java/in/silive/clime/Models/GetCityLocation.java
@@ -4,12 +4,21 @@ import android.content.Context;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.util.Log;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.List;
 import java.util.Locale;
 
@@ -18,35 +27,36 @@ import java.util.Locale;
  */
 public class GetCityLocation {
     public static double lat, lng;
-   public static Location location;
+    public static Location location;
 
     public GetCityLocation(Context c, String city) {
-        Log.d("TAG","GetCity Location called");
-        getAddressFromLocation(city, c, new Handler());
+        Log.d("TAG", "GetCity Location called");
+        //getAddressFromLocation(city, c, new Handler());
+        new fetchLatLongFromService(city).execute();
 
     }
 
-    public  Location getLocation() {
+    public Location getLocation() {
         return location;
     }
 
-    public static void setLocation(Location location) {
+    public void setLocation(Location location) {
         GetCityLocation.location = location;
     }
 
-    public  double getLat() {
+    public double getLat() {
         return lat;
     }
 
-    public static void setLat(double lat) {
+    public void setLat(double lat) {
         GetCityLocation.lat = lat;
     }
 
-    public  double getLng() {
+    public double getLng() {
         return lng;
     }
 
-    public static void setLng(double lng) {
+    public void setLng(double lng) {
         GetCityLocation.lng = lng;
     }
 
@@ -99,5 +109,115 @@ public class GetCityLocation {
             }
         };
         thread.start();
+    }
+
+    public class fetchLatLongFromService extends
+            AsyncTask<Void, Void, StringBuilder> {
+        String place;
+
+
+        public fetchLatLongFromService(String place) {
+            super();
+            this.place = place;
+
+        }
+
+        @Override
+        protected void onCancelled() {
+            // TODO Auto-generated method stub
+            super.onCancelled();
+            this.cancel(true);
+        }
+
+        @Override
+        protected StringBuilder doInBackground(Void... params) {
+
+            try {
+                URL H_url = new URL("http://maps.googleapis.com/maps/api/geocode/json?address=" + this.place + "&sensor=false");
+                HttpURLConnection H_connection = (HttpURLConnection) H_url.openConnection();
+
+                Log.d("TAG", "connection");
+                H_connection.setRequestMethod("GET");
+                H_connection.setRequestProperty("Content-Type", "application/json;charset=UTF-8");
+                H_connection.connect();
+                BufferedReader H_bufferedReader = new BufferedReader(new InputStreamReader(H_connection.getInputStream()));
+                StringBuilder jsonResults = new StringBuilder();
+                String a = "";
+                while (((a = H_bufferedReader.readLine()) != null)) {
+                    jsonResults.append(a);
+                }
+
+                return jsonResults;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+
+        }
+
+        @Override
+        protected void onPostExecute(StringBuilder result) {
+            super.onPostExecute(result);
+            try {
+                JSONObject jsonObject;
+                try {
+                    jsonObject = new JSONObject(result.toString());
+
+                    lng = ((JSONArray) jsonObject.get("results")).getJSONObject(0)
+                            .getJSONObject("geometry").getJSONObject("location")
+                            .getDouble("lng");
+
+
+                    lat = ((JSONArray) jsonObject.get("results")).getJSONObject(0)
+                            .getJSONObject("geometry").getJSONObject("location")
+                            .getDouble("lat");
+                    Log.d("TAG", "location inside GetCityLocation " + lat + " " + lng);
+                    location.setLatitude(lat);
+                    location.setLongitude(lng);
+                    setLocation(location);
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+                /*JSONObject jsonObj = new JSONObject(result.toString());
+                JSONArray resultJsonArray = jsonObj.getJSONArray("results");
+
+                // Extract the Place descriptions from the results
+                // resultList = new ArrayList<String>(resultJsonArray.length());
+
+                JSONObject before_geometry_jsonObj = resultJsonArray
+                        .getJSONObject(0);
+
+                JSONObject geometry_jsonObj = before_geometry_jsonObj
+                        .getJSONObject("geometry");
+
+                JSONObject location_jsonObj = geometry_jsonObj
+                        .getJSONObject("location");
+
+                String lat_helper = location_jsonObj.getString("lat");
+                lat = Double.valueOf(lat_helper);
+                location.setLatitude(lat);
+
+
+                String lng_helper = location_jsonObj.getString("lng");
+                lng = Double.valueOf(lng_helper);
+                location.setLongitude(lng);
+                setLocation(location);
+
+
+                LatLng point = new LatLng(lat, lng);
+
+
+            } catch (JSONException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+
+            }*/
+        }
     }
 }
